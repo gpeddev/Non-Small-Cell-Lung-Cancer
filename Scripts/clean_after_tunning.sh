4f6e0ac84535ec663be63d0fdf8875590a029197
gpeddev <33454964+gpeddev@users.noreply.github.com>
2022-08-02 09:57:09 +0300

    Adjusting for storing images

--- Scripts/clean_after_tunning.sh
+++ Scripts/clean_after_tunning.sh
@@ -5,6 +5,10 @@ rm -rf ./Output/Logs/*
 
 rm -rf ./Output/Models/*
 
-rm -rf ./Output/Images/*
+rm -rf ./Output/Images/0/*
+rm -rf ./Output/Images/1/*
+rm -rf ./Output/Images/2/*
+rm -rf ./Output/Images/3/*
+rm -rf ./Output/Images/4/*
 
 rm -rf ./Data/09_TrainingSet_VAE1/*
--- Scripts/save_last_model.sh
+++ Scripts/save_last_model.sh
@@ -11,6 +11,12 @@ mkdir "./Output/Logs"
 
 mv "./Output/Images" "./StoreResults/$NOW/"
 mkdir "./Output/Images"
+mkdir "./Output/Images/0"
+mkdir "./Output/Images/1"
+mkdir "./Output/Images/2"
+mkdir "./Output/Images/3"
+mkdir "./Output/Images/4"
+
 
 mv "./Output/DatasetSplits" "./StoreResults/$NOW/"
 mkdir "./Output/DatasetSplits"
