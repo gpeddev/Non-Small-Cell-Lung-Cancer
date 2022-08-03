083da3a84de382b63fd1ed89aae79f5f0448bf07
gpeddev <33454964+gpeddev@users.noreply.github.com>
2022-08-04 00:59:19 +0300

    fixed paths

--- Models/VAE_1/VAE_1_parameters.py
+++ Models/VAE_1/VAE_1_parameters.py
@@ -1,9 +1,9 @@
 # basic hyperparameters of our data
 learning_rate = 0.00001
-latent_dimensions = 64
+latent_dimensions = 128
 filters_number = 64
 kernels_number = 3
 stride = 2
 batch_sz = 32
 kl_weight = 1
-reconstruction_weight = 1000000
+reconstruction_weight = 1
