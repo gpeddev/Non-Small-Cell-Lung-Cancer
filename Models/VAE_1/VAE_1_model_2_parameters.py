f66a5cc5b65709e6962cf9f259acb733d6d0b1be
gpeddev <33454964+gpeddev@users.noreply.github.com>
2022-08-06 09:28:19 +0300

    hyperparameters

--- Models/VAE_1/VAE_1_model_2_parameters.py
+++ Models/VAE_1/VAE_1_model_2_parameters.py
@@ -1,9 +1,13 @@
 # basic hyperparameters of our data
-learning_rate = 0.00001
-latent_dimensions = 64
-filters_number = 32
-kernels_number = 3
-stride = 2
-batch_sz = 32
-kl_weight = 1
+learning_rate = 1e-05
+latent_dimensions = 32
+filters_number = 16
+kl_weight = 0.1
+
+
+
+# ignore-considered stable
 reconstruction_weight = 1
+batch_sz = 32
+stride = 2
+kernels_number = 3
