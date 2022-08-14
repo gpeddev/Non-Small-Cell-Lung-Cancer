c1b2848f52511c404150bbdba07159406379fb02
gpeddev <33454964+gpeddev@users.noreply.github.com>
2022-08-14 21:28:14 +0300

    pooling

--- Pooling/Pooling.py
+++ Pooling/Pooling.py
@@ -9,22 +9,24 @@ from SupportCode.datasets_support import preprocess_data
 
 
 
-def pooling(encoder_path, vaes_latent_space):
+def pooling(encoder_path, vaes_latent_space, source_images):
 
     results = []
     model_directory = encoder_path
-    encoder_1 = tf.keras.models.load_model(model_directory + '/VAE_encoder_1', compile=False)
-    encoder_2 = tf.keras.models.load_model(model_directory + '/VAE_encoder_2', compile=False)
-    encoder_3 = tf.keras.models.load_model(model_directory + '/VAE_encoder_3', compile=False)
-    encoder_4 = tf.keras.models.load_model(model_directory + '/VAE_encoder_4', compile=False)
-    encoder_5 = tf.keras.models.load_model(model_directory + '/VAE_encoder_5', compile=False)
+    encoder_1 = tf.keras.models.load_model(model_directory + '/VAE_encoder_1.h5', compile=False)
+    encoder_2 = tf.keras.models.load_model(model_directory + '/VAE_encoder_2.h5', compile=False)
+    encoder_3 = tf.keras.models.load_model(model_directory + '/VAE_encoder_3.h5', compile=False)
+    encoder_4 = tf.keras.models.load_model(model_directory + '/VAE_encoder_4.h5', compile=False)
+    encoder_5 = tf.keras.models.load_model(model_directory + '/VAE_encoder_5.h5', compile=False)
     encoders = [encoder_1, encoder_2, encoder_3, encoder_4, encoder_5]
 
+    i = 1
     for enc in encoders:
+        print("Pooling on encoder "+str(i))
         patient_dict = {}
-        for patient in os.listdir(CropTumor):
+        for patient in os.listdir(source_images):
             # load all slices of patient
-            patient_slices = preprocess_data(CropTumor, [patient])
+            patient_slices = preprocess_data(source_images, [patient])
             # apply encoder to each slice
 
             patient_feature_array = np.empty((0, vaes_latent_space))
@@ -37,14 +39,10 @@ def pooling(encoder_path, vaes_latent_space):
             # store outcome to dictionary {patient_name : feature numpy array [number of features, length to latent space]}
             average_pc = np.average(patient_feature_array, axis=0)
             max_pc = np.max(patient_feature_array, axis=0)
-            patient_dict[patient] = np.concatenate([average_pc, max_pc])
+            patient_dict[patient.rsplit(".")[0]] = np.concatenate([average_pc, max_pc])
         results.append(patient_dict)
+        i=i+1
+    print("Pooling finished")
+    return results
 
-    results
-
-
-
-    # apply average per column
-    # apply max per column
-    # concatenate to feature
-    # add to dictionary a pair {patient: extracted feature}
+#rslt=pooling("./BestResults/VAE_1/Model_1/19_2022-08-10_01_53_19/Models", 256,CropTumor)
