469fccbed45eeab87837e5af03e8583487764523
gpeddev <33454964+gpeddev@users.noreply.github.com>
2022-08-16 07:10:16 +0300

    vae 2 model 1

--- Models/VAE_2/VAE_2_model_1_parameters.py
+++ Models/VAE_2/VAE_2_model_1_parameters.py
@@ -1,8 +1,8 @@
 # basic hyperparameters of our data
-learning_rate = 0.00001
+learning_rate = 0.0001
 filters_number = 32
-latent_dimensions = 256
-kl_weight = 0.125
+latent_dimensions = 64
+kl_weight = 1
 
 
 
