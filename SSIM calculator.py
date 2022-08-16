f25e8a984805c1c5daca44a58b5290985b0b01fa
gpeddev <33454964+gpeddev@users.noreply.github.com>
2022-08-16 16:04:22 +0300

    ssim

--- SSIM calculator.py
+++ SSIM calculator.py
@@ -10,16 +10,23 @@ from SupportCode.Paths import CropTumor
 from skimage.metrics import structural_similarity as ssim
 
 
-def evaluate_vae_ssim(vae_path, dataset_path):
+def evaluate_vae_ssim(vae_path, dataset_path,val=True):
     """evaluate vaes ssim."""
-
+    if val==True:
         # load names of test datasets for each model
-    val_dataset_1 = np.load(dataset_path + "/val_dataset_fold_1.npy")
-    val_dataset_2 = np.load(dataset_path + "/val_dataset_fold_2.npy")
-    val_dataset_3 = np.load(dataset_path + "/val_dataset_fold_3.npy")
-    val_dataset_4 = np.load(dataset_path + "/val_dataset_fold_4.npy")
-    val_dataset_5 = np.load(dataset_path + "/val_dataset_fold_5.npy")
-    datasets = [val_dataset_1, val_dataset_2, val_dataset_3, val_dataset_4, val_dataset_5]
+        val_dataset_1 = np.load(dataset_path + "/val_dataset_fold_1.npy")
+        val_dataset_2 = np.load(dataset_path + "/val_dataset_fold_2.npy")
+        val_dataset_3 = np.load(dataset_path + "/val_dataset_fold_3.npy")
+        val_dataset_4 = np.load(dataset_path + "/val_dataset_fold_4.npy")
+        val_dataset_5 = np.load(dataset_path + "/val_dataset_fold_5.npy")
+        datasets = [val_dataset_1, val_dataset_2, val_dataset_3, val_dataset_4, val_dataset_5]
+    else:
+        test_dataset_1 = np.load(dataset_path + "/test_dataset_fold_1.npy")
+        test_dataset_2 = np.load(dataset_path + "/test_dataset_fold_2.npy")
+        test_dataset_3 = np.load(dataset_path + "/test_dataset_fold_3.npy")
+        test_dataset_4 = np.load(dataset_path + "/test_dataset_fold_4.npy")
+        test_dataset_5 = np.load(dataset_path + "/test_dataset_fold_5.npy")
+        datasets = [test_dataset_1, test_dataset_2, test_dataset_3, test_dataset_4, test_dataset_5]
 
 
 
@@ -46,31 +53,35 @@ def evaluate_vae_ssim(vae_path, dataset_path):
     return sum(results)/5
 
 
-# Model 1
+# my computer couldnt handle all of them. Run out of memory. Uncomment if you want to run all together.
+
+# x1=evaluate_vae_ssim("./BestResults/VAE_1/Model_1a/17_2022-08-13_10_26_21/Models",
+#                     "./BestResults/VAE_1/Model_1a/17_2022-08-13_10_26_21/DatasetSplits", val=True)
+# print("model_1 kl=1")
+# print(x1)
+#
+# x2=evaluate_vae_ssim("./BestResults/VAE_1/Model_1a/18_2022-08-13_10_27_30/Models",
+#                     "./BestResults/VAE_1/Model_1a/18_2022-08-13_10_27_30/DatasetSplits", val=True)
+# print("model_1 kl=0.5")
+# print(x2)
+
+# x3=evaluate_vae_ssim("./BestResults/VAE_1/Model_1a/19_2022-08-13_10_28_14/Models",
+#                     "./BestResults/VAE_1/Model_1a/19_2022-08-13_10_28_14/DatasetSplits", val=True)
+# print("model_1 kl=0.25")
+# print(x3)
+#
+# x4=evaluate_vae_ssim("./BestResults/VAE_1/Model_1a/44_2022-08-14_11_53_46/Models",
+#                     "./BestResults/VAE_1/Model_1a/44_2022-08-14_11_53_46/DatasetSplits", val=True)
+# print("model_1 kl=0.125")
+# print(x4)
 
 
-
-x=evaluate_vae_ssim("./BestResults/VAE_1/Model_1/17_2022-08-10_01_47_44/Models",
-                    "./BestResults/VAE_1/Model_1/17_2022-08-10_01_47_44/DatasetSplits",
-                    dataset_to_test="val")
+x1=evaluate_vae_ssim("./BestResults/VAE_1/Model_1a/17_2022-08-13_10_26_21/Models",
+                     "./BestResults/VAE_1/Model_1a/17_2022-08-13_10_26_21/DatasetSplits", val=False)
 print("model_1 kl=1")
-print(x)
+print(x1)
 
-x=evaluate_vae_ssim("./BestResults/VAE_1/Model_1/18_2022-08-10_01_50_29/Models",
-                    "./BestResults/VAE_1/Model_1/18_2022-08-10_01_50_29/DatasetSplits",
-                    dataset_to_test="val")
-print("model_1 kl=0.5")
-print(x)
-
-x=evaluate_vae_ssim("./BestResults/VAE_1/Model_1/19_2022-08-10_01_53_19/Models",
-                    "./BestResults/VAE_1/Model_1/19_2022-08-10_01_53_19/DatasetSplits",
-                    dataset_to_test="val")
-print("model_1 kl=0.25")
-print(x)
-
-x=evaluate_vae_ssim("./BestResults/VAE_1/Model_1/20_2022-08-10_01_55_25/Models",
-                    "./BestResults/VAE_1/Model_1/20_2022-08-10_01_55_25/DatasetSplits",
-                    dataset_to_test="val")
-print("model_1 kl=0.125")
-print(x)
-
+x1=evaluate_vae_ssim("./BestResults/VAE_1/Model_1/17_2022-08-10_01_47_44/Models",
+                     "./BestResults/VAE_1/Model_1/17_2022-08-10_01_47_44/DatasetSplits", val=False)
+print("model_1 kl=1")
+print(x1)
