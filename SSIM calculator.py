810b877eaee317f6c821cfc9972e4529979822f5
gpeddev <33454964+gpeddev@users.noreply.github.com>
2022-08-21 19:16:41 +0300

    SSIM calculator

--- SSIM calculator.py
+++ SSIM calculator.py
@@ -9,32 +9,34 @@ from SupportCode.datasets_support import preprocess_data
 from SupportCode.Paths import CropTumor,CroppedWindow
 from skimage.metrics import structural_similarity as ssim
 
+# point to the model you want to calculate SSIM score
+# if you want to test VAE 2 category use CroppedWindow else use CropTumor for VAE 1
+# for testing VAE on validation dataset set val=True or for testing dataset set val = False
+model_path="./BestResults/VAE_1/Model_1/17_2022-08-10_01_47_44"
 
-def evaluate_vae_ssim(vae_path, dataset_path,image_path,val):
+def evaluate_vae_ssim(vae_path,image_path,val):
     """evaluate vaes ssim."""
-    if val==True:
+    if val is True:
         # load names of test datasets for each model
-        val_dataset_1 = np.load(dataset_path + "/val_dataset_fold_1.npy")
-        val_dataset_2 = np.load(dataset_path + "/val_dataset_fold_2.npy")
-        val_dataset_3 = np.load(dataset_path + "/val_dataset_fold_3.npy")
-        val_dataset_4 = np.load(dataset_path + "/val_dataset_fold_4.npy")
-        val_dataset_5 = np.load(dataset_path + "/val_dataset_fold_5.npy")
+        val_dataset_1 = np.load(vae_path + "/DatasetSplits/val_dataset_fold_1.npy")
+        val_dataset_2 = np.load(vae_path + "/DatasetSplits/val_dataset_fold_2.npy")
+        val_dataset_3 = np.load(vae_path + "/DatasetSplits/val_dataset_fold_3.npy")
+        val_dataset_4 = np.load(vae_path + "/DatasetSplits/val_dataset_fold_4.npy")
+        val_dataset_5 = np.load(vae_path + "/DatasetSplits/val_dataset_fold_5.npy")
         datasets = [val_dataset_1, val_dataset_2, val_dataset_3, val_dataset_4, val_dataset_5]
     else:
-        test_dataset_1 = np.load(dataset_path + "/test_dataset_fold_1.npy")
-        test_dataset_2 = np.load(dataset_path + "/test_dataset_fold_2.npy")
-        test_dataset_3 = np.load(dataset_path + "/test_dataset_fold_3.npy")
-        test_dataset_4 = np.load(dataset_path + "/test_dataset_fold_4.npy")
-        test_dataset_5 = np.load(dataset_path + "/test_dataset_fold_5.npy")
+        test_dataset_1 = np.load(vae_path + "/DatasetSplits/test_dataset_fold_1.npy")
+        test_dataset_2 = np.load(vae_path + "/DatasetSplits/test_dataset_fold_2.npy")
+        test_dataset_3 = np.load(vae_path + "/DatasetSplits/test_dataset_fold_3.npy")
+        test_dataset_4 = np.load(vae_path + "/DatasetSplits/test_dataset_fold_4.npy")
+        test_dataset_5 = np.load(vae_path + "/DatasetSplits/test_dataset_fold_5.npy")
         datasets = [test_dataset_1, test_dataset_2, test_dataset_3, test_dataset_4, test_dataset_5]
 
-
-
-    vae_1 = tf.keras.models.load_model(vae_path + '/VAE_1.h5', compile=False)
-    vae_2 = tf.keras.models.load_model(vae_path + '/VAE_2.h5', compile=False)
-    vae_3 = tf.keras.models.load_model(vae_path + '/VAE_3.h5', compile=False)
-    vae_4 = tf.keras.models.load_model(vae_path + '/VAE_4.h5', compile=False)
-    vae_5 = tf.keras.models.load_model(vae_path + '/VAE_5.h5', compile=False)
+    vae_1 = tf.keras.models.load_model(vae_path + '/Models/VAE_1.h5', compile=False)
+    vae_2 = tf.keras.models.load_model(vae_path + '/Models/VAE_2.h5', compile=False)
+    vae_3 = tf.keras.models.load_model(vae_path + '/Models/VAE_3.h5', compile=False)
+    vae_4 = tf.keras.models.load_model(vae_path + '/Models/VAE_4.h5', compile=False)
+    vae_5 = tf.keras.models.load_model(vae_path + '/Models/VAE_5.h5', compile=False)
     VAEs = [vae_1, vae_2, vae_3, vae_4, vae_5]
 
 
@@ -52,33 +54,8 @@ def evaluate_vae_ssim(vae_path, dataset_path,image_path,val):
     return sum(results)/5
 
 
-# my computer couldnt handle all of them. Run out of memory. Uncomment if you want to run all together.
-
-
-x1=evaluate_vae_ssim("./BestResults/VAE_2/Model_1/17_2022-08-15_19_47_42/Models",
-                     "./BestResults/VAE_2/Model_1/17_2022-08-15_19_47_42/DatasetSplits",
-                     image_path=CroppedWindow,
-                     val=True)
-print("model_1 kl=1")
-print(x1)
-
-x1=evaluate_vae_ssim("./BestResults/VAE_2/Model_1/20_2022-08-15_19_51_41/Models",
-                     "./BestResults/VAE_2/Model_1/20_2022-08-15_19_51_41/DatasetSplits",
-                     image_path=CroppedWindow,
+# my computer couldnt more than one since each evaluate_vae_ssim loads 5 models so I had to run it one by one
+x1=evaluate_vae_ssim(model_path,
+                     image_path=CropTumor,
                      val=False)
-print("model_1 kl=0.125")
-print(x1)
-
-x1=evaluate_vae_ssim("./BestResults/VAE_2/Model_1/22_2022-08-16_04_02_50/Models",
-                     "./BestResults/VAE_2/Model_1/22_2022-08-16_04_02_50/DatasetSplits",
-                     image_path=CroppedWindow,
-                     val=True)
-print("model_1 kl=0.5")
-print(x1)
-
-x1=evaluate_vae_ssim("./BestResults/VAE_2/Model_1/23_2022-08-15_19_35_09/Models",
-                     "./BestResults/VAE_2/Model_1/23_2022-08-15_19_35_09/DatasetSplits",
-                     image_path=CroppedWindow,
-                     val=True)
-print("model_1 kl=0.25")
 print(x1)
